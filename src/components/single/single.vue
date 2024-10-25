51b607d914bbf551043c46ae1577344cd1cb3e82
Kaviilee <jiaweilee95@126.com>
2024-10-26 00:26:52 +0800

    perf: 历史记录

--- src/components/single/single.vue
+++ src/components/single/single.vue
@@ -62,7 +62,7 @@ export default {
 
                     User.isLogin() && postHistory({
                         source_type: appKey,
-                        source_id: this.id,
+                        source_id: ~~this.id,
                         link: location.href,
                         title: this.post.post_title,
                     });
